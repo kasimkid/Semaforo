5b62493a67d56851094bcfb86950b8634bece22f
Rafael Araujo <kasim.85.ra@gmail.com>
2023-07-28 12:30:09 -0400

    Cambios en Archivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const App = () => {
 
-  const [ligth, setLigth] = useState ('')
+  const [ligth, setLigth] = useState('')
 
   const click = (luz) => {
     setLigth(luz)
@@ -10,29 +10,29 @@ const App = () => {
 
 
   }
-  
-
-  return(
-     <>
-     <div className="poste"></div>
-     <div className="contenedor">
-      <div id={ligth == 'rojo' ? "on" : 'false'} className="red-ligth" onClick={()=>click('rojo')}></div>
-      <div id={ligth == 'yellow' ? "on" : 'false'} className="yellow-ligth" onClick={()=>click('yellow')}></div>
-      <div id={ligth == 'green' ? "on" : 'false'} className="green-ligth" onClick={()=>click('green')}></div>
-    </div>
 
 
+  return (
+    <>
+      <div className="poste"></div>
+      <div className="contenedor">
+        <div id={ligth == 'rojo' ? "on" : 'false'} className="red-ligth" onClick={() => click('rojo')}></div>
+        <div id={ligth == 'yellow' ? "on" : 'false'} className="yellow-ligth" onClick={() => click('yellow')}></div>
+        <div id={ligth == 'green' ? "on" : 'false'} className="green-ligth" onClick={() => click('green')}></div>
+      </div>
 
-    <div className="boton">
-      <button className="random">Change</button>
-    <button className="purple">Purple</button>
-    </div>
 
 
-     </>
+      <div className="boton">
+        <button className="random">Change</button>
+        <button className="purple">Purple</button>
+      </div>
+
+
+    </>
   );
 
-    }
+}
 
 
 export default App;
